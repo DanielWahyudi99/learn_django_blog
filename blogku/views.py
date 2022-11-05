f0c29688f6a69fe780bdd6e04c1149075d70ce66
daniel <wahyudi_daniel@yahoo.com>
2022-11-05 09:34:28 +0700

    commit berhasil add app dan template

---
+++ blogku/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.template import loader
+
+
+def index(request):
+    return HttpResponse("Hellloo World")
+
+def firstpage(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
